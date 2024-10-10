b8abf2baf97c1d4ea0867fd1eb45c6d4d246bbbb
Enzo Lavaud <lavaud.enzo@gmail.com>
2024-10-10 10:08:13 +0200

    Init

---
+++ src/test/java/mds/tp/api/hackr/tpApi/TpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mds.tp.api.hackr.tpApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
