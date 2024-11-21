22fcef30b207a5fbc65a854474fce9eae5fb4a6c
Enzo Lavaud <lavaud.enzo@gmail.com>
2024-11-21 14:39:32 +0100

    Correction Mails

--- src/main/java/mds/tp/api/hackr/controllers/EmailController.java
+++ src/main/java/mds/tp/api/hackr/controllers/EmailController.java
@@ -23,7 +23,7 @@ public class EmailController {
     }
 
     @PostMapping("/spam-mail")
-        public void spamEmail(@RequestParam("email") String email, @RequestParam("nbEmail") int nbEmail, @RequestParam("subject") String subject,
+    public ResponseEntity<String> spamEmail(@RequestParam("email") String email, @RequestParam("nbEmail") int nbEmail, @RequestParam("subject") String subject,
                           @RequestParam("text") String text, @RequestParam("gifUrl") String gifUrl,
                               @RequestParam("from") String from, HttpSession httpSession) {
         MDC.put("userLogged", httpSession.getAttribute("userLogged").toString());
@@ -32,7 +32,7 @@ public class EmailController {
             this.emailService.sendEmail(email, subject, text, gifUrl, from);
         }
         log.info("Spam mail sent to {}, {} times", email, nbEmail);
-        ResponseEntity.ok("Mails sent");
+        return ResponseEntity.ok("Mails sent");
     }
 
     @GetMapping("/verify-mail")
